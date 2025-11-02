744ec7bf3e419d1fd975fa82709bbde36cdccca6
Lucas Baudin <lbaudin@gnome.org>
2025-11-02 13:33:48 +0100

    build: upgrade typst to 0.14

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use typst::syntax::package::PackageSpec;
 use typst::syntax::{FileId, Source};
 use typst::text::{Font, FontBook};
 use typst::utils::LazyHash;
-use typst::Library;
+use typst::{Library, LibraryExt};
 use typst_kit::fonts::{FontSearcher, FontSlot};
 
 /// Main interface that determines the environment for Typst.
